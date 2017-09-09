173fa9d34bd405f6a85e6cbadfa31d6c973a80d8
alexkraut <alexandre.kraut@epfedu.fr>
2017-09-09 19:23:14 +0200

    + decrementation

--- src/fr/epf/deadpoules/controller/DeleteMemberServlet.java
+++ src/fr/epf/deadpoules/controller/DeleteMemberServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import fr.epf.deadpoules.model.Member;
 import fr.epf.deadpoules.persistence.MemberDao;
+import fr.epf.deadpoules.persistence.PromotionDao;
 
 @WebServlet("/delete-member")
 public class DeleteMemberServlet extends HttpServlet {
@@ -19,11 +20,15 @@ public class DeleteMemberServlet extends HttpServlet {
 	@Inject
 	private MemberDao memberDao;
 	
+	@Inject
+	private PromotionDao promotionDao;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Long memberId = Long.valueOf(request.getParameter("memberId"));
 		Member deleteMember = memberDao.findOne(memberId);
-
+		deleteMember.getPromotion().incrementNumMembers();
+		promotionDao.update(deleteMember.getPromotion());
 		memberDao.delete(deleteMember);
 		
 		response.sendRedirect("dashboard");
--- src/fr/epf/deadpoules/persistence/PromotionDao.java
+++ src/fr/epf/deadpoules/persistence/PromotionDao.java
@@ -36,13 +36,6 @@ public class PromotionDao {
 	public long count() {
 	 return (long)em.createQuery("SELECT COUNT(*) FROM Promotion").getSingleResult();
 	}
-	
-	public List<Integer> countMembers() {
-		
-		List<Integer> listCountMembers;
-		listCountMembers = em.createQuery("SELECT COUNT(*) FROM Member GROUP BY promotion_id").getResultList();
-		return listCountMembers;
-		}
 
 	public List<Promotion> findAll() {
 		List<Promotion> promotions = new ArrayList<>();
